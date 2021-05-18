734adcc2bd501927184868068007682a168f3a3f
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-05-18 15:13:19 +0200

    push correction

--- main.js
+++ main.js
@@ -10,11 +10,14 @@
 
 let exo1div = document.getElementById('content');
 let exo1h1 = document.getElementsByTagName('h1')[0];
-let exo1h2 = document.getElementsByTagName('h2')[0]
+let exo1h2 = document.getElementsByTagName('h2')[0];
 let exo1p = document.getElementsByTagName('p')[0];
 
 function bordure(e) {
-    e.target.style.border = 'black solid 1px';
+    if (e.target.id == 'content') {
+        e.target.style.border = 'black solid 1px';
+    }
+    
 }
 
 function grasRouge(e) {
@@ -24,7 +27,7 @@ function grasRouge(e) {
 
 function surligner(e) {
     e.target.style.background = 'yellow';
-    e.target.style.textDecoration = 'none';
+    // e.target.style.textDecoration = 'none';
 }
 
 function effacer(e) {
@@ -35,6 +38,7 @@ exo1div.addEventListener('click', bordure);
 exo1h1.addEventListener('click', grasRouge);
 exo1h2.addEventListener('click',surligner);
 exo1p.addEventListener('click', effacer);
+
 
 // ### 2. 
 // - Créer une variable contenant le texte suivant : "Il est possible de passer des parametres dans la function utilisé dans l'écouteur d'événement et nous verrons ça dans le prochain exercice !"
@@ -50,3 +54,14 @@ function changer(e) {
 }
 
 exo2p.addEventListener('mouseover',changer)
+
+
+//correction exo2
+
+let sec = document.querySelector('section');
+let p2 = document.querySelector('section p');
+let variable = "Il est possible de passer des parametres dans la function utilisé dans l'écouteur d'événement et nous verrons ça dans le prochain exercice !";
+sec.style.height = "200px";
+sec.addEventListener('mouseover',()=>{
+    p2.innerText = variable
+})
